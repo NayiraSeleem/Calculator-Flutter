90399462cc96a962f5315dfae7d0e585f7c946c4
NayiraSeleem <https://github.com/NayiraSeleem>
2023-04-07 16:17:58 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:calculator/myCalculator.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyCalculator());
+}
+
+class MyCalculator extends StatelessWidget {
+  const MyCalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      debugShowCheckedModeBanner: false,
+      home: Calculator(),
+    );
+  }
+}
